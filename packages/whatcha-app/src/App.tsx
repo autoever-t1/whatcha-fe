0161b61a372e80f9e6f3f5242df42e06057d949b
Sangyeon Park <53224839+ektto1041@users.noreply.github.com>
2025-01-12 15:49:34 +0900

    chore: common 패키지 기본 설정 (#1)

--- packages/whatcha-app/src/App.tsx
+++ packages/whatcha-app/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Button } from "@karponents/xmas";
+import Logo from "@common/assets/logo.png";
 
 function App() {
   return (
     <>
       <h1>app</h1>
       <Button>asdf</Button>
+      <img src={Logo} />
     </>
   );
 }
