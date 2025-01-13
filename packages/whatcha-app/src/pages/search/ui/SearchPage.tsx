569ee6a736e449000e819eeadec5e32099b2b6a7
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2025-01-13 11:34:26 +0900

    feat: add SearchHeader, ConditionBox

--- packages/whatcha-app/src/pages/search/ui/SearchPage.tsx
+++ packages/whatcha-app/src/pages/search/ui/SearchPage.tsx
@@ -1,5 +1,28 @@
+import { SearchHeader } from "./SearchHeader";
 import styles from "./SearchPage.module.css";
+import { TabItem } from "./TabItem";
 
 export function SearchPage() {
-  return <div className={styles.container}>SearchPage</div>;
+  return (
+    <div className={styles.container}>
+      <SearchHeader />
+      <div className={styles.content}>
+        <div className={styles["tab-list"]}>
+          {[
+            "차종",
+            "모델",
+            "가격",
+            "주행거리",
+            "연식",
+            "연료",
+            "색상 계열",
+            "옵션",
+          ].map((tab, i) => (
+            <TabItem key={i}>{tab}</TabItem>
+          ))}
+        </div>
+        <div className={styles["condition-box"]}></div>
+      </div>
+    </div>
+  );
 }
