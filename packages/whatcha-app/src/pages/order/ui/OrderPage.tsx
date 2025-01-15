9088951378776b7c7645b63ca01cfe76ff1832fa
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2025-01-15 09:52:04 +0900

    feat: add Order-pay Page

--- packages/whatcha-app/src/pages/order/ui/OrderPage.tsx
+++ packages/whatcha-app/src/pages/order/ui/OrderPage.tsx
@@ -1,5 +1,34 @@
+import { MainHeader } from "@shared/main-header";
 import styles from "./OrderPage.module.css";
+import { useCallback, useEffect, useState } from "react";
+import { ContentBox } from "@shared/content-box";
+import { BillContent } from "@shared/bill-content";
+import { ProgressBar } from "./ProgressBar";
+import { BottomButton } from "@shared/bottom-button";
 
 export function OrderPage() {
-  return <div className={styles.container}>OrderPage</div>;
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((p) => (p + 1) % 5);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <MainHeader title="주문 현황" />
+      <div className={styles.content}>
+        <ProgressBar progress={progress} />
+        <ContentBox title="주문금액" position="bottom">
+          <BillContent price={30000000} />
+        </ContentBox>
+      </div>
+      <BottomButton>잔금 결제하기</BottomButton>
+    </div>
+  );
 }
