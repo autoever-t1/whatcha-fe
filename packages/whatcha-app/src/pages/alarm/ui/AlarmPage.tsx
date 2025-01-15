cef16c7e5a9085ac9866f88acad42667309144c8
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2025-01-15 21:20:27 +0900

    feat: AlarmPage

--- packages/whatcha-app/src/pages/alarm/ui/AlarmPage.tsx
+++ packages/whatcha-app/src/pages/alarm/ui/AlarmPage.tsx
@@ -1,5 +1,30 @@
+import { MainHeader } from "@shared/main-header";
 import styles from "./AlarmPage.module.css";
+import { useNavigate } from "react-router";
+import { useCallback } from "react";
+import { AlarmItem } from "./AlarmItem";
 
 export function AlarmPage() {
-  return <div className={styles.container}></div>;
+  const navigate = useNavigate();
+
+  const handleClickBackButton = useCallback(() => {
+    navigate("/mypage");
+  }, [navigate]);
+
+  return (
+    <div className={styles.container}>
+      <MainHeader
+        title="입고 알림 신청 내역"
+        onClickBack={handleClickBackButton}
+      />
+      <div className={styles.content}>
+        <AlarmItem
+          alarm={{
+            model: "쏘나타 하이브리드 르블랑",
+            expireDate: "2025-01-15",
+          }}
+        />
+      </div>
+    </div>
+  );
 }
