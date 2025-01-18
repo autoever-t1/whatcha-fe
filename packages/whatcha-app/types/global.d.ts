305c80356706896bb033d2b3e501cd85620ab13d
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2025-01-18 12:12:36 +0900

    chore: AndroidInterface 설정

--- packages/whatcha-app/types/global.d.ts
+++ packages/whatcha-app/types/global.d.ts
@@ -1,5 +1,23 @@
 interface Window {
   AndroidInterface: {
-    getAccessToken: () => string;
+    getToken: () => string;
+    getEmail: () => string;
+    getName: () => string;
+    getUserId: () => number;
+    getAddress: () => string;
+    getPhone: () => string;
+    getBudgetMin: () => number;
+    getBudgetMax: () => number;
+    getNotificationAgree: () => boolean;
+    getLocationAgree: () => boolean;
+    getPrefer1: () => string;
+    getPrefer2: () => string;
+    getPrefer3: () => string;
+    setPrefer1: (newPrefer1: string) => void;
+    setPrefer2: (newPrefer2: string) => void;
+    setPrefer3: (newPrefer3: string) => void;
+    setBudgetMin: (newBudgetMin: number) => void;
+    setBudgetMax: (newBudgetMax: number) => void;
+    log: (message: string) => void;
   };
 }
