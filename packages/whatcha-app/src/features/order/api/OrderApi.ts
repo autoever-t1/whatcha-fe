cdc61473cf57c4a2d8ea26e8f18329d569c4334a
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2025-01-19 21:49:35 +0900

    fix: 지도 경로 API 분리

--- packages/whatcha-app/src/features/order/api/OrderApi.ts
+++ packages/whatcha-app/src/features/order/api/OrderApi.ts
@@ -4,6 +4,7 @@ import {
   DepositDTO,
   OrderListItemDTO,
   OrderSheetDTO,
+  PathReqDTO,
 } from "../model/types";
 
 export const createOrder = async (depositInfo: DepositDTO) => {
@@ -49,6 +50,12 @@ export const chooseMethod = async (orderId: number) => {
   const response = await authAxios.put<void>(
     `api/order/${orderId}/deliveryService`
   );
+
+  return response.data;
+};
+
+export const getPathAPI = async (pathData: PathReqDTO) => {
+  const response = await authAxios.post("api/order/path", pathData);
 
   return response.data;
 };
