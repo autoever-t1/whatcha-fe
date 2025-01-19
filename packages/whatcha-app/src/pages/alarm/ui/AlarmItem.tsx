892649b308311faedba84e47c3544933553b18ca
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2025-01-20 00:05:12 +0900

    feat: AlarmList API

--- packages/whatcha-app/src/pages/alarm/ui/AlarmItem.tsx
+++ packages/whatcha-app/src/pages/alarm/ui/AlarmItem.tsx
@@ -1,21 +1,19 @@
+import { AlarmListItemDTO } from "@/features/alarm";
 import styles from "./AlarmItem.module.css";
 import CloseIcon from "@common/assets/icons/close.svg";
 
-interface Alarm {
-  model: string;
-  expireDate: string;
-}
-
 interface AlarmItemProps {
-  alarm: Alarm;
+  alarm: AlarmListItemDTO;
 }
 
 export function AlarmItem({ alarm }: AlarmItemProps) {
   return (
     <div className={styles.container}>
       <div className={styles.box}>
-        <p className="font-b-md">{alarm.model}</p>
-        <p className="font-r-sm color-gray-400">~ {alarm.expireDate}</p>
+        <p className="font-b-md">{alarm.modelName}</p>
+        <p className="font-r-sm color-gray-400">
+          ~ {alarm.alertExpirationDate}
+        </p>
         <button className={styles["icon-button"]}>
           <img src={CloseIcon} alt="Close" />
         </button>
